bb25bcd17eb1b769353b3456180bfcc8c95d89f5
Nevemore <senderpl12@gmail.com>
2023-05-23 00:30:16 +0200

    too much to describe

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Input, Button, Col, ImageCard, SimpleGrid } from '@mantine/core';
+import { Input, Button, Col, SimpleGrid, Grid } from '@mantine/core';
+import {ImageCard} from '../components/ImageCard';
 
 function SearchPage() {
     const history = useHistory;
@@ -8,25 +9,30 @@ function SearchPage() {
     const [games, setGames] = useState([]);
 
     const handleSearch = async () => {
+        //replace spaces with %20
+        setSearchTerm(searchTerm.replace(' ', '%20'));
         const res = await fetch(`/api/games?name=${searchTerm}`);
         const data = await res.json();
         const games = data.data;
-
-        // Dla każdej gry wykonaj zapytanie do Steam API, aby uzyskać szczegóły gry
-        const gamesWithDetails = await Promise.all(games.map(async game => {
-            const steamRes = await fetch(`/api/games/${game.appid}`);
-            if (steamRes.status === 200) {
+        const gamesWithDetails = await Promise.all(
+            games.map(async (game) => {
+                const steamRes = await fetch(`/api/games/${game.appid}`);
                 const steamData = await steamRes.json();
+                let price = '';
+                if (steamData.price_overview && steamData.price_overview.final_formatted) {
+                    price = steamData.price_overview.final_formatted;
+                }
                 return {
                     ...game,
-                    header_image: steamData[game.appid].data.header_image
+                    header_image: steamData.header_image || '',
+                    price,
                 };
-            }
-
-        }));
+            })
+        );
 
         setGames(gamesWithDetails);
     };
+
 
     const handleCardClick = (id) => {
         history.push(`/games/${id}`);
@@ -41,18 +47,21 @@ function SearchPage() {
     />
     <Button onClick={handleSearch}>Search</Button>
 
-        <SimpleGrid cols={4} spacing="lg" style={{ marginTop: '20px' }}>
-    {games.map((game) => (
-        <Col key={game._id} span={3}>
-    <ImageCard
-        radius="md"
-        image={game.header_image}
-        title={game.name}
-        onClick={() => handleCardClick(game.appid)}
-        />
-        </Col>
-    ))}
-    </SimpleGrid>
+            <SimpleGrid cols={4} spacing="lg" style={{ marginTop: '20px' }}>
+                {games.map((game) =>
+                        game && (
+                            <div key={game._id}>
+                                <ImageCard
+                                    link={`/games/${game.appid}`}
+                                    image={game.header_image}
+                                    title={game.name}
+                                    price={game.price}  // Cena jest teraz stringiem
+                                />
+                            </div>
+                        )
+                )}
+            </SimpleGrid>
+
     </div>
 );
 }
