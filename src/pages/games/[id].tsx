bf8a85fc39b4a71a97cdea103afe5b3481805a4b
Nevemore <senderpl12@gmail.com>
2023-06-10 02:21:36 +0200

    changed domain

--- src/pages/games/[id].tsx
+++ src/pages/games/[id].tsx
@@ -60,7 +60,7 @@ function GamePage({user}: GamePageProps) {
                 });
 
                 const { convertedAmount: convertedAmountPLN } = await currencyResPLN.json();
-                response.data.price_overview.final_formatted = (convertedAmountPLN * 1.5).toFixed(2) + ' PLN';
+                response.data.price_overview.final_formatted = (convertedAmountPLN * 1.5).toFixed(2);
 
                 const currencyResUSD = await fetch('/api/convertUSD', {
                     method: 'POST',
@@ -118,11 +118,11 @@ function GamePage({user}: GamePageProps) {
                         <Paper p="md" shadow="md" radius="sm" style={{height:"100%", width:"25%", float:"right", marginRight:"15%"}}>
                             <Title order={1}>{game.name}</Title>
                             <Text size="xl" style={{ marginBottom: '20px' }}>
-                                {game.price_overview ? `Price: ${game.price_overview.final_formatted_usd + ' USD'}` : 'Price not available'}
+                                {game.price_overview ? `Price: ${game.price_overview.final_formatted + ' PLN'}` : 'Price not available'}
                             </Text>
                             <Button className={"snipcart-add-item"}
                                     data-item-id={game.steam_appid}
-                                    data-item-price={game.price_overview ? game.price_overview.final_formatted_usd : 'Price not available'}
+                                    data-item-price={game.price_overview ? game.price_overview.final_formatted : 'Price not available'}
                                     data-item-url={`/games/${id}`}
                                     data-item-description={game.detailed_description}
                                     data-item-image={game.header_image}
