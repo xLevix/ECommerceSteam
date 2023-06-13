5178dafbccee7c1417ac4bd103305a5b5f98c8ed
Nevemore <senderpl12@gmail.com>
2023-06-13 18:29:13 +0200

    trying to ssr

--- src/pages/api/convertUSD.js
+++ src/pages/api/convertUSD.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
         const response = await fetch('https://api.exchangerate-api.com/v4/latest/ARS');
         const data = await response.json();
         const exchangeRate = data.rates.USD;
-        const amountInUSD = amount * exchangeRate;
+        const amountInUSD = (amount * exchangeRate)*1.5;
         return res.status(200).json({ convertedAmount: amountInUSD.toFixed(2) });
     } catch (error) {
         return res.status(500).json({ msg: 'Internal server error' });
